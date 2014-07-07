2ac3be14ffdc5a78b528ddd6420bb51f28c8a773
Kudryashov <hekpomamohtehok@gmail.com>
2014-07-07 12:47:23 +0300

    add possibility update and destroy books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   end
 
   def show
-    @books = find_book
+    @book = find_book
   end
 
   def new
@@ -14,9 +14,29 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new book_params 
+    @book = Book.new book_params
     @book.save 
+    flash[:notice] = "#{@book.title} was successfully created."
     redirect_to books_url  
+  end
+
+  def edit
+    @book = find_book
+  end
+
+  def update
+    @book = find_book
+    @book.update_attributes!(book_params)
+    @book.save
+    flash[:notice] = "#{@book.title} was successfully updated."
+    redirect_to @book
+  end
+ 
+  def destroy
+    @book = find_book
+    @book.destroy
+    flash[:notice] = "Book '#{@book.title}' deleted."
+    redirect_to books_url
   end
 
   private
